4615d2258b512eefc90541d332efc9d2f31de4ff
sammyatuh <42051074+sammyatuh@users.noreply.github.com>
2018-09-10 15:31:17 -1000

    Update Test2.java

--- src/com/sammy/test/git/Test2.java
+++ src/com/sammy/test/git/Test2.java
@@ -3,6 +3,7 @@ public class Test2 {
 	public static void main(String[] args){
 	  System.out.println("Hi there 2");
 	  System.out.println("Have a wonderful day!");
+	  System.out.println("One more line");
 
 	}
 }
