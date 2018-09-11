370081ab909c06d607d87dc1f480bde3ed21d7fa
sammyatuh <42051074+sammyatuh@users.noreply.github.com>
2018-09-10 15:32:40 -1000

    Update Test1.java

--- src/com/sammy/test/git/Test1.java
+++ src/com/sammy/test/git/Test1.java
@@ -4,5 +4,7 @@ public class Test1 {
 	public static void main(String[] args){
 	  System.out.println(hello);
 	  System.out.println("Hello World Hawaii!");
+	  System.out.println("one more!");
+
 	}
 }
