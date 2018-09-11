a70de3935cac48b27c4c5071ea6b80b32a7e669a
sammyatuh <42051074+sammyatuh@users.noreply.github.com>
2018-09-11 13:09:37 -1000

    Update Test2.java

--- src/com/sammy/test/git/Test2.java
+++ src/com/sammy/test/git/Test2.java
@@ -2,7 +2,7 @@ package com.sammy.test.git;
 public class Test2 {
 	public static void main(String[] args){
 	  System.out.println("Hi there 2");
-	  System.out.println("Have a wonderful day!");
+	  //once more
 	  //delete again
 	}
 }
